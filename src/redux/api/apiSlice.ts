58900a1c9c67b27ca69525a86444a0ebb4b321fa
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-16 18:59:13 +0600

    backend link updated.

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,8 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    // baseUrl: 'https://server-bookshelf.vercel.app/api/v1',
-    baseUrl: 'http://localhost:8080/api/v1',
+    baseUrl: 'https://server-bookshelf.vercel.app/api/v1',
     prepareHeaders: (headers) => {
       const token = JSON.parse(localStorage.getItem('auth') as string);
       headers.set('authorization', `${token?.accessToken}`);
