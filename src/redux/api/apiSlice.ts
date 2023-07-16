b9fe903c97f2f20d5a89af10e0ebf408a254be9e
Md Minhazur Rahman Rafi <rafi.rahman03@yahoo.com>
2023-07-16 15:21:46 +0600

    end point change

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/api/v1' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://server-bookshelf.vercel.app/api/v1',
+  }),
   tagTypes: ['reviews', 'books'],
   endpoints: () => ({}),
 });
